825c2a3d4b8791dfb4b096ea01ff90b8e20d42d4
benjababe <bengohzy@gmail.com>
2022-03-03 14:09:59 +0800

    Add rtsp stream from RPI (untested)

--- Android/app/src/main/java/com/ntu/cz3004/group4/androidremote/fragments/LeftColFragment.java
+++ Android/app/src/main/java/com/ntu/cz3004/group4/androidremote/fragments/LeftColFragment.java
@@ -4,13 +4,14 @@ import static com.ntu.cz3004.group4.androidremote.Constants.A_MOVE_BACKWARD;
 import static com.ntu.cz3004.group4.androidremote.Constants.A_MOVE_FORWARD;
 import static com.ntu.cz3004.group4.androidremote.bluetooth.BluetoothService.STATE_CONNECTED;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -20,15 +21,28 @@ import com.ntu.cz3004.group4.androidremote.R;
 import com.ntu.cz3004.group4.androidremote.bluetooth.BluetoothService;
 import com.ntu.cz3004.group4.androidremote.bluetooth.Packet;
 
+import org.videolan.libvlc.LibVLC;
+import org.videolan.libvlc.Media;
+import org.videolan.libvlc.MediaPlayer;
+import org.videolan.libvlc.util.VLCVideoLayout;
+
 public class LeftColFragment extends Fragment {
     ListView lvConsole;
     TextView txtRoboStatus, txtRoboDirection, txtRoboPosition;
-    ImageView imgRecog;
     ImageButton btnAccel, btnReverse;
+    LibVLC libVLC;
+    MediaPlayer mediaPlayer;
+    VLCVideoLayout vidRPI;
+    Button btnVid;
 
     ArrayAdapter<String> consoleArrayAdapter;
 
     BluetoothService bluetoothService;
+
+    static final String testURL = "rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mp4";
+    static final String url = "rtsp://192.168.4.4:8554/unicast";
+
+    boolean vidToggle = false;
 
     public LeftColFragment() {
     }
@@ -50,16 +64,43 @@ public class LeftColFragment extends Fragment {
         txtRoboStatus = view.findViewById(R.id.txtRoboStatus);
         txtRoboDirection = view.findViewById(R.id.txtRoboDirection);
         txtRoboPosition = view.findViewById(R.id.txtRoboPosition);
-        imgRecog = view.findViewById(R.id.imgRecog);
         btnAccel = view.findViewById(R.id.btnAccel);
         btnReverse = view.findViewById(R.id.btnReverse);
+        vidRPI = view.findViewById(R.id.vidRPI);
+        btnVid = view.findViewById(R.id.btnVid);
 
         btnAccel.setOnClickListener(this::accelerate);
         btnReverse.setOnClickListener(this::reverse);
+        btnVid.setOnClickListener(this::toggleVideo);
 
         consoleArrayAdapter = new ArrayAdapter<>(getContext(), R.layout.item_message);
         lvConsole.setAdapter(consoleArrayAdapter);
         lvConsole.setSelection(consoleArrayAdapter.getCount() - 1);
+
+        libVLC = new LibVLC(this.getContext());
+        mediaPlayer = new MediaPlayer(libVLC);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        mediaPlayer.attachViews(vidRPI, null, false, false);
+
+        Media media = new Media(libVLC, Uri.parse(url));
+        media.setHWDecoderEnabled(true, false);
+
+        mediaPlayer.setMedia(media);
+        media.release();
+    }
+
+    public void toggleVideo(View view) {
+        vidToggle = !vidToggle;
+
+        if (vidToggle)
+            mediaPlayer.play();
+        else
+            mediaPlayer.pause();
     }
 
     public void accelerate(View view) {
